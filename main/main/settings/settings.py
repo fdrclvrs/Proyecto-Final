3287c4c3c702d69c5dbaaedcf755a624e800e354
1r4ky <tynchop@gmail.com>
2023-12-13 05:36:21 -0300

    se hizo todo lo del manual hasta la parte de la integracion con bases de datos debido a que da error, ya se hizo consulta estoy esperando respuesta

--- main/main/settings/settings.py
+++ main/main/settings/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'usuario.Usuario'
+
 
 # Application definition
 
@@ -38,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'apps.posts',
+    'apps.contacto',
+    'apps.usuario'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIR = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
 
